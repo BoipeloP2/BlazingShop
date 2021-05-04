da089a370bf87011d438ca7537748392a642f9e1
BPhiri <BPhiri@BPHIRI-DEV>
2021-05-04 20:07:21 +0200

    Added event callbacks and dependency injection

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -2,87 +2,43 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace BlazingShop.Client.Services.ProductService
 {
     public class ProductService : IProductService
     {
+        private readonly HttpClient _http;
+
+        public event Action OnChange;
+
         public List<Product> Products { get; set; } = new List<Product>();
 
-        public void LoadProducts()
+        public ProductService(HttpClient http)
         {
-            Products = new List<Product>
+            _http = http;
+        }
+        public async Task LoadProducts(string categoryUrl = null)
+        {
+            if (categoryUrl == null)
             {
-
-        new Product
-        {
-            Id = 1,
-            CategoryId = 1,
-            Title = "The hitchhiker",
-            Description = "The hitchhiker's guide to the galaxy",
-            Image = "https://en.wikipedia.org/wiki/File:H2G2_UK_front_cover.jpg#/media/File:H2G2_UK_front_cover.jpg",
-            Price  = 9.99m,
-            OriginalPrice = 19.99m,
-
-        },
-         new Product
-        {
-            Id = 2,
-            CategoryId = 2,
-            Title = "The Half-life2",
-            Description = "HALF=LIFE the best game",
-            Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-life_2_cover.jgp",
-            Price  = 9.99m,
-            OriginalPrice = 19.99m,
-
-        },
-          new Product
-        {
-            Id = 3,
-            CategoryId = 3,
-            Title = "The Half-life3",
-            Description = "HALF=LIFE the best game",
-            Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-life_2_cover.jgp",
-            Price  = 9.99m,
-            OriginalPrice = 19.99m,
-
-        },
-           new Product
-        {
-            Id = 4,
-            CategoryId = 1,
-            Title = "The Half-life4",
-            Description = "HALF=LIFE the best game",
-            Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-life_2_cover.jgp",
-            Price  = 9.99m,
-            OriginalPrice = 19.99m,
-
-        },
-            new Product
-        {
-            Id = 5,
-            CategoryId = 2,
-            Title = "The Half-life5",
-            Description = "HALF=LIFE the best game",
-            Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-life_2_cover.jgp",
-            Price  = 9.99m,
-            OriginalPrice = 19.99m,
-
-        },
-             new Product
-        {
-            Id = 6,
-            CategoryId = 3,
-            Title = "The Half-life6",
-            Description = "HALF=LIFE the best game",
-            Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-life_2_cover.jgp",
-            Price  = 9.99m,
-            OriginalPrice = 19.99m,
-
+                Products = await _http.GetFromJsonAsync<List<Product>>("api/product");
+            }
+            else
+            {
+                Products = await _http.GetFromJsonAsync<List<Product>>($"api/product/Category/{categoryUrl}");
+            }
+            OnChange.Invoke();
         }
-            };
+
+        public async Task<Product> GetProduct(int id)
+        {
+            return await _http.GetFromJsonAsync<Product>($"api/product/{id}");
         }
+
+       
     }
 
 }
---
+++ Server/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using BlazingShop.Server.Services.ProductService;
+using BlazingShop.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productSerivce;
+
+        public ProductController(IProductService productSerivce)
+        {
+            _productSerivce = productSerivce;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Product>>> GetAllProducts()
+        {
+            return Ok(await _productSerivce.GetAllProducts());
+        }
+
+        [HttpGet("Category/{categoryUrl}")]
+        public async Task<ActionResult<List<Product>>> GetProductsByCategory( string categoryUrl)
+        {
+            return Ok(await _productSerivce.GetProductsByCategory(categoryUrl));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return Ok(await _productSerivce.GetProduct(id));
+        }
+    }
+}
